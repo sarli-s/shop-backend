6d2255dd3d79264e6fb8e993a9cfeb23c4aedad3
AyalaOnGit <arh67637@gmail.com>
2026-02-23 15:37:50 +0200

    Generate

--- Entitys/Rating.cs
+++ Entitys/Rating.cs
@@ -1,16 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace Entitys
+namespace Entitys;
+
+public partial class Rating
 {
-    public partial class Rating
-    {
-        public int RatingId { get; set; }
-        public string Host { get; set; }
-        public string Method { get; set; }
-        public string Path { get; set; }
-        public string Referer { get; set; }
-        public string UserAgent { get; set; }
-        public DateTime? RecordDate { get; set; }
-    }
+    public int RatingId { get; set; }
+
+    public string? Host { get; set; }
+
+    public string? Method { get; set; }
+
+    public string? Path { get; set; }
+
+    public string? Referer { get; set; }
+
+    public string? UserAgent { get; set; }
+
+    public DateTime? RecordDate { get; set; }
 }
